763a98780fd47169814f22cbd8c5d1644dbc82bb
Marta Żaczkowska <marta.zaczkowska@gmail.com>
2019-07-13 15:58:55 +0200

    added scass

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,17 @@
 import moment from 'moment';
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+const welcome = (name, age) => {
+    console.log (`Witaj ${name}, masz ${age} lat!`);
+}
+
+welcome(`Artur`, 31)
+
+const navigationSwitcher = document.querySelector(`.navigation__switcher--js`)
+
+navigationSwitcher.addEventListener(`click`, (e) => {
+    const navigationList = document.querySelector(`.navigation__list--js`);
+    navigationList.classList.toggle(`navigation__list--visible`);
+
+});
